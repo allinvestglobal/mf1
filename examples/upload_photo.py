c72af3566ce27ede3defbebdfc3d6fa2bcafa92e
allinvestglobal <53827177+allinvestglobal@users.noreply.github.com>
2019-12-04 12:00:47 +0100

    Add files via upload

---
+++ examples/upload_photo.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Use text editor to edit the script and type in valid Instagram username/password
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '1.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '2.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '3.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '4.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '5.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '6.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '7.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '8.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("amigoosdemadrid", "20million")
+InstagramAPI.login()  # login
+
+photo_path = '9.png'
+caption = ""
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
